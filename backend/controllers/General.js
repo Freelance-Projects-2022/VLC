6afc11c9e3f3c206b4690226499cbdec5e881bc0
Iyad Saadeh <93885250+IyadSaadeh@users.noreply.github.com>
2022-04-29 17:51:42 +0300

    fix comma

--- backend/controllers/General.js
+++ backend/controllers/General.js
@@ -1,9 +1,9 @@
 const connection = require("../database/db");
 
 const createNewGeneral=(req, res)=>{
-const {car_no,car_type,vin,color,model,vin_tr,vin_tl,vin_br,vin_bl,body_note,engine_test,gear_test,back_acss} = req.body;
-    const query = "INSERT INTO User (car_no,car_type,vin,color,model,vin_tr,vin_tl,vin_br,vin_bl,body_note,engine_test,gear_test,back_acss) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)";
-   const data = [car_no,car_type,vin,color,model,vin_tr,vin_tl,vin_br,vin_bl,body_note,engine_test,gear_test,back_acss];
+const {vin_tr,vin_tl,vin_br,vin_bl,body_note,engine_test,gear_test,back_acss} = req.body;
+    const query = "INSERT INTO User (vin_tr,vin_tl,vin_br,vin_bl,body_note,engine_test,gear_test,back_acss) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)";
+   const data = [vin_tr,vin_tl,vin_br,vin_bl,body_note,engine_test,gear_test,back_acss];
     connection.query(query,data, (err, result) => {
       if (err) {
         return res
@@ -17,22 +17,5 @@ const {car_no,car_type,vin,color,model,vin_tr,vin_tl,vin_br,vin_bl,body_note,eng
       });
     });
 }
-const getAllGeneral=(req, res)=>{
 
-}
-const getGeneralByCarNo=(req, res)=>{
 
-}
-const getGeneralByOrderNo=(req, res)=>{
-
-}
-const getGeneralByVinNo=(req, res)=>{
-
-}
-
-// const getAllOrder=(req, res)=>{
-
-// }
-module.exports ={
-
-}
