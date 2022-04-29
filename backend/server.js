eabe71e93d0b73943103c8f5655b4a5b998b86b2
Haitham Alulaimi <haithamalulaimi@gmail.com>
2022-04-29 14:34:13 +0300

    create login router

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,25 @@
+//====================================================//Require
 const express = require("express");
+const PORT = 5000;
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
-const db = require("./database/db");
-
 app.use(cors());
-
 app.use(express.json());
 
-const PORT = 5000;
 
+//====================================================//import database
+const connection = require("./database/db");
+
+//====================================================// Import Routers
+const loginRouter = require("./routes/login");
+
+//====================================================// Routes Middleware
+
+app.use("/login", loginRouter);
+
+
+//====================================================// Handles any other endpoints [unassigned - endpoints]
 app.listen(PORT, () => {
   console.log(`SERVER WORKING ON PORT: ${PORT}`);
 });
