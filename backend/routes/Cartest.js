98b2703cb02c86f6a60aeccac2ae53c08fa4f3fc
Haitham Alulaimi <haithamalulaimi@gmail.com>
2022-04-30 01:55:05 +0300

    hybrid car test

--- backend/routes/Cartest.js
+++ backend/routes/Cartest.js
@@ -2,10 +2,11 @@
 const express = require("express");
 
 //====================================================//Require Functions
-const { petrolCarTest } = require("../controllers/Cartest");
+const { petrolCarTest, HybridCarTest } = require("../controllers/Cartest");
 const CarTestRouter = express.Router();
 
 CarTestRouter.post("/petrolCar", petrolCarTest);
+CarTestRouter.post("/hybridCar", HybridCarTest);
 
 //====================================================// module.exports
 
