599ef12a236004e33cdf2a4810a2efd738d8c631
Haitham Alulaimi <haithamalulaimi@gmail.com>
2022-04-30 04:03:54 +0300

    last commit for today

--- backend/controllers/Cartest.js
+++ backend/controllers/Cartest.js
@@ -1,7 +1,8 @@
 //====================================================//Require
 const connection = require("../database/db");
+const { uuid } = require("uuidv4");
 
-//====================================================//Petrol Car Test
+//====================================================//Create Petrol Car Test
 const petrolCarTest = (req, res) => {
   const {
     vin_tr,
@@ -39,7 +40,7 @@ const petrolCarTest = (req, res) => {
   });
 };
 
-//====================================================//hybrid car test
+//====================================================// Create hybrid car test
 
 const HybridCarTest = (req, res) => {
   const {
@@ -84,6 +85,51 @@ const HybridCarTest = (req, res) => {
   });
 };
 
+//====================================================// Create order_test
+
+// const orderTest = (req, res) => {
+//   const {
+//     engine_test,
+//     transmission,
+//     srs,
+//     abs_system,
+//     ac,
+//     hybrid_system,
+//     hv_battery,
+//     eleectric_system,
+//     emc,
+//     soh,
+//     note,
+//   } = req.body;
+//   const query =
+//     "INSERT INTO order_test (`car_no`, `car_type`, `car_vin`, `car_color`, `car_model`, `car_order_no`, `car_notes`, `test_price`, `car_petrol_test`, `car_hybrid_test`) VALUES (?,?,?,?,?,?,?,?)";
+//   const data = [
+//     engine_test,
+//     transmission,
+//     srs,
+//     abs_system,
+//     ac,
+//     hybrid_system,
+//     hv_battery,
+//     eleectric_system,
+//     emc,
+//     soh,
+//     note,
+//   ];
+//   connection.query(query, data, (err, result) => {
+//     if (err) {
+//       return res
+//         .status()
+//         .json({ success: false, message: "Server Error", error: err.message });
+//     }
+//     return res.status(200).json({
+//       success: true,
+//       message: "Successfully Add Car Test",
+//       result: result,
+//     });
+//   });
+// };
+
 //====================================================//get all Petrol Cars where is_deleted =0
 const getPetrolCars = (req, res) => {
   const query = "SELECT *  FROM vlc.petrol_car where is_deleted =0;";
@@ -115,7 +161,7 @@ const getCarByCarNo = (req, res) => {
       if (result.length === 0) {
         return res.status(404).json({
           success: false,
-          massage: "No Car Test Found",
+          massage: "No Car Test Found For This Car No.",
           err: err,
         });
         // result are the data returned by mysql server
@@ -134,7 +180,7 @@ const getCarByCarNo = (req, res) => {
           } else {
             return res.status(200).json({
               success: true,
-              massage: `No Car Test Found`,
+              massage: `No Car Test Found For This Car No.`,
               result: result,
             });
           }
@@ -151,49 +197,49 @@ const getCarByCarNo = (req, res) => {
 };
 
 // //====================================================//get car depend on vin
-// const getCarByCarVin = (req, res) => {
-//   const carNo = req.body.carVin;
-//   const data = [carNo];
-//   const query = " SELECT *  FROM order_test where car_no=? And is_deleted =0";
-//   connection.query(query, data, (err, result) => {
-//     try {
-//       if (result.length === 0) {
-//         return res.status(404).json({
-//           success: false,
-//           massage: "No test belong for this number",
-//           err: err,
-//         });
-//         // result are the data returned by mysql server
-//       } else {
-//         const carId = result[0].id;
-//         const data = [carId];
-//         const query =
-//           "  SELECT *  FROM order_test as ot  Join hybrid_car as hc on ot.id=hc.id Join petrol_car pc on ot.id=pc.id where ot.is_deleted = 0";
-//         connection.query(query, data, (err, result) => {
-//           if (err) {
-//             return res.status(404).json({
-//               success: false,
-//               massage: "No test belong for this number",
-//               err: err,
-//             });
-//           } else {
-//             return res.status(200).json({
-//               success: true,
-//               massage: `car with No => ${carId} test info`,
-//               results: result,
-//             });
-//           }
-//         });
-//       }
-//     } catch {
-//       res.status(500).json({
-//         success: false,
-//         massage: "server error",
-//         err: err,
-//       });
-//     }
-//   });
-// };
+const getCarByCarVin = (req, res) => {
+  const carVin = req.body.car_vin;
+  const data = [carVin];
+  const query = " SELECT *  FROM order_test where car_vin=? And is_deleted =0";
+  connection.query(query, data, (err, result) => {
+    try {
+      if (result.length === 0) {
+        return res.status(404).json({
+          success: false,
+          massage: "No Car Test Found For This Car Vin",
+          err: err,
+        });
+        // result are the data returned by mysql server
+      } else {
+        const carId = result[0].id;
+        const data = [carId, carId, carId];
+        const query =
+          "select * from hybrid_car hc join petrol_car pc on hc.id=? and pc.id=? join order_test ot on ot.id=?";
+        connection.query(query, data, (err, result) => {
+          if (err) {
+            return res.status(500).json({
+              success: false,
+              massage: "Server Error",
+              err: err,
+            });
+          } else {
+            return res.status(200).json({
+              success: true,
+              massage: `No Car Test Found For This Car Vin`,
+              result: result,
+            });
+          }
+        });
+      }
+    } catch {
+      res.status(500).json({
+        success: false,
+        massage: "server error",
+        err: err,
+      });
+    }
+  });
+};
 
 //====================================================//get all hybrid Cars  where is_deleted =0
 const gethybridCars = (req, res) => {
@@ -279,4 +325,5 @@ module.exports = {
   deletePetrolCar,
   deletehybridcar,
   getCarByCarNo,
+  getCarByCarVin,
 };
